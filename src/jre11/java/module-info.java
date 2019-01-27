b75ce50d8fd94b40c1c9433652b4e77ae8d49782
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:14:24 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,9 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.easypiechart.EasyPieChartPageConfigurator;
-import com.jwebmp.plugins.easypiechart.implementations.EasyPieChartExclusionsModule;
-
 module com.jwebmp.plugins.easypiechart {
 	exports com.jwebmp.plugins.easypiechart;
 
@@ -16,10 +10,10 @@ module com.jwebmp.plugins.easypiechart {
 	requires com.jwebmp.plugins.easingeffects;
 	requires com.jwebmp.guicedinjection;
 
-	provides IPageConfigurator with EasyPieChartPageConfigurator;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.easypiechart.EasyPieChartPageConfigurator;
 
-	provides IGuiceScanModuleExclusions with EasyPieChartExclusionsModule;
-	provides IGuiceScanJarExclusions with EasyPieChartExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.easypiechart.implementations.EasyPieChartExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.easypiechart.implementations.EasyPieChartExclusionsModule;
 
 	opens com.jwebmp.plugins.easypiechart to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
