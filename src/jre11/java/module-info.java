35a71014c13306776ff5e63a8c66ddc203de47d3
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:33 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -2,18 +2,18 @@ module com.jwebmp.plugins.easypiechart {
 	exports com.jwebmp.plugins.easypiechart;
 
 	requires com.jwebmp.core;
-	requires com.jwebmp.logmaster;
+	requires com.guicedee.logmaster;
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;
 	requires com.jwebmp.plugins.easingeffects;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.easypiechart.EasyPieChartPageConfigurator;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.easypiechart.implementations.EasyPieChartExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.easypiechart.implementations.EasyPieChartExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.easypiechart.implementations.EasyPieChartExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.easypiechart.implementations.EasyPieChartExclusionsModule;
 
 	opens com.jwebmp.plugins.easypiechart to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
